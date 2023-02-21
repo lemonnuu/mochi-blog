029502b1b591db5f4654128a146ff12f554ce05f
lemonnuu <480438117@qq.com>
2023-02-21 10:58:54 +0800

    v-bind 合并行为与删除 v-on.native 修饰符

--- docs/.vitepress/sidebar/vue3Migration.js
+++ docs/.vitepress/sidebar/vue3Migration.js
@@ -11,6 +11,8 @@ const vue3MigrationSidebar = [
       { text: 'v-model', link: '/_NOTES/Vue3 Migration/Template Directives/001_v-model' },
       { text: 'key 使用改变', link: '/_NOTES/Vue3 Migration/Template Directives/002-key-attribute' },
       { text: 'v-if 与 v-for 优先级', link: '/_NOTES/Vue3 Migration/Template Directives/003_v-if-v-for' },
+      { text: 'v-bind 合并行为', link: '/_NOTES/Vue3 Migration/Template Directives/004_v-bind' },
+      { text: 'v-on.native 移除', link: '/_NOTES/Vue3 Migration/Template Directives/005_v-on-native-modifier-removed' },
     ],
   },
 ]
